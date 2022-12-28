3036f17e30148b450d5fd95abffb3ba25c19e6a4
vivisuke <ntsuda@master.email.ne.jp>
2022-12-29 05:00:08 +0900

    0017 ◎ C 12/27 12/28 0.001│ToDo│指定位置着手関数 put(x, y, color) 実装

--- SuperTicTacToe/scripts/MainScene.gd
+++ SuperTicTacToe/scripts/MainScene.gd
@@ -3,9 +3,12 @@ extends Node2D
 const N_VERT = 9
 const N_HORZ = 9
 const NEXT_LOCAL_BOARD = 0
+const MARU = 1
+const BATSU = 0
 
 func _ready():
-	init_board()
+	#init_board()
+	#put(2, 2, MARU)
 	pass # Replace with function body.
 
 func init_board():
@@ -17,3 +20,12 @@ func init_board():
 			$Board/TileMapBG.set_cell(x, y, NEXT_LOCAL_BOARD)
 			$Board/TileMapGlobal.set_cell(x, y, -1)
 	pass
+
+func put(x, y, col):
+	$Board/TileMapLocal.set_cell(x, y, col)
+	var x3 : int = x % 3
+	var y3 : int = y % 3
+	for gy in range(N_VERT/3):
+		for gx in range(N_HORZ/3):
+			var c = NEXT_LOCAL_BOARD if gx == x3 && gy == y3 else -1
+			$Board/TileMapBG.set_cell(gx, gy, c)
