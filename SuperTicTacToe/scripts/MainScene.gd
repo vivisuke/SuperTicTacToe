a4912c190bea53b6301d2de4f044485a63002b78
vivisuke <ntsuda@master.email.ne.jp>
2022-12-29 08:58:52 +0900

    0019 ◎ C 12/29 12/29 0.001│ToDo│手番表示更新 0020 ◎ C 12/29 12/29 0.001│ToDo│ローカル内で三目並んだ場合はグローバルボードを更新

--- SuperTicTacToe/scripts/MainScene.gd
+++ SuperTicTacToe/scripts/MainScene.gd
@@ -17,6 +17,7 @@ func _ready():
 	BOARD_ORG_Y = $Board/TileMapLocal.global_position.y
 	BOARD_ORG = Vector2(BOARD_ORG_X, BOARD_ORG_Y)
 	init_board()
+	update_next_label()
 	#put(2, 2, MARU)
 	pass # Replace with function body.
 
@@ -30,7 +31,8 @@ func init_board():
 			$Board/TileMapBG.set_cell(x, y, NEXT_LOCAL_BOARD)
 			$Board/TileMapGlobal.set_cell(x, y, -1)
 	pass
-
+func update_next_label():
+	$MessLabel.text = "次は%sの手番です。" % ("Ｏ" if next_color == MARU else "Ｘ")
 func can_put_local(x : int, y : int):
 	return $Board/TileMapBG.get_cell(x, y) == NEXT_LOCAL_BOARD
 func is_empty(x : int, y : int):
@@ -38,12 +40,30 @@ func is_empty(x : int, y : int):
 func put(x : int, y : int, col):
 	if !is_empty(x, y): return
 	$Board/TileMapLocal.set_cell(x, y, col)
+	# 次着手可能ローカルボード強調
 	var x3 : int = x % 3
 	var y3 : int = y % 3
 	for gy in range(N_VERT/3):
 		for gx in range(N_HORZ/3):
 			var c = NEXT_LOCAL_BOARD if gx == x3 && gy == y3 else -1
 			$Board/TileMapBG.set_cell(gx, gy, c)
+func get_color(x : int, y : int):
+	return $Board/TileMapLocal.get_cell(x, y)
+func is_three_stones(x : int, y : int):		# 三目並んだか？
+	var x3 : int = x % 3
+	var x0 : int = x - x3		# ローカルボード内左端座標
+	var y3 : int = y % 3
+	var y0 : int = y - y3		# ローカルボード内上端座標
+	if get_color(x0, y) == get_color(x0+1, y) && get_color(x0, y) == get_color(x0+2, y):
+		return true;			# 横方向に三目並んだ
+	if get_color(x, y0) == get_color(x, y0+1) && get_color(x, y0) == get_color(x, y0+2):
+		return true;			# 縦方向に三目並んだ
+	if x3 == y3:		# ＼斜め方向チェック
+		if get_color(x0, y0) == get_color(x0+1, y0+1) && get_color(x0, y0) == get_color(x0+2, y0+2):
+			return true;			# ＼斜め方向に三目並んだ
+	if x3 == 2 - y3:		# ／斜め方向チェック
+		if get_color(x0, y0+2) == get_color(x0+1, y0+1) && get_color(x0, y0+2) == get_color(x0+2, y0):
+			return true;			# ／斜め方向に三目並んだ
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -61,5 +81,8 @@ func _input(event):
 			var gy = int(pos.y) / 3
 			if !can_put_local(gx, gy): return
 			put(pos.x, pos.y, next_color)
+			if is_three_stones(pos.x, pos.y):
+				$Board/TileMapGlobal.set_cell(gx, gy, next_color)
 			next_color = (MARU + BATSU) - next_color
+			update_next_label()
 	pass
