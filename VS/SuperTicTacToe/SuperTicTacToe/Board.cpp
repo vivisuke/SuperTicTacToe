2683a45b0c3a6b8f6351074cc856ef371bc8a29f
vivisuke <ntsuda@master.email.ne.jp>
2023-01-22 08:48:54 +0900

    0095 ◎ C 01/22 01/22 0.001│ToDo│Board：ローカル・グローバルボード変数・初期化

--- VS/SuperTicTacToe/SuperTicTacToe/Board.cpp
+++ VS/SuperTicTacToe/SuperTicTacToe/Board.cpp
@@ -10,4 +10,11 @@
 #include "Board.h"
 
 Board::Board() {
+	init();
+}
+void Board::init() {
+	for(int i = 0; i != BD_SIZE; ++i)
+		m_board[i] = EMPTY;
+	for(int i = 0; i != GBD_SIZE; ++i)
+		m_gboard[i] = EMPTY;
 }
--- VS/SuperTicTacToe/SuperTicTacToe/Board.h
+++ VS/SuperTicTacToe/SuperTicTacToe/Board.h
@@ -9,7 +9,22 @@
 
 #pragma once
 
+//	全盤面サイズ
+#define		N_HORZ		9
+#define		N_VERT		9
+#define		BD_SIZE		(N_HORZ*N_VERT)
+#define		GBD_SIZE	(N_HORZ/3*N_VERT/3)
+
+#define		EMPTY		0
+#define		MARU		1
+#define		BATSU		(-1)
+
 class Board {
 public:
 	Board();
+public:
+	void	init();
+private:
+	char	m_board[BD_SIZE];
+	char	m_gboard[GBD_SIZE];
 };
